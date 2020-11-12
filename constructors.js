2c462fbcd35c3cec7420be3a698e76c0e754d1a4
nickloverde <nick.loverde@gmail.com>
2020-11-12 15:22:41 -0800

    Finished assignment

--- constructors.js
+++ constructors.js
@@ -16,6 +16,11 @@
 
 // Code here
 
+function CarFactory(make, model){
+  this.make = make
+  this.model = model
+}
+
 ////////// PROBLEM 2 //////////
 
 // Do not edit the code below.
@@ -34,6 +39,10 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
+
+  const bob = new Employee('Bob', "bob@gmail.com", "01-02-98")
+
+
   
   ////////// PROBLEM 3 //////////
   
@@ -57,6 +66,18 @@ function Employee(name, email, hireDate) {
   */
   
   // Code here
+
+  function Car(make, model, year, move){
+    this.make = make
+    this.model = model
+    this.year = year
+    this.move = 0
+    this.moveCar = function(){
+      this.move + 10
+      return this.move
+    }
+  }
+  
   
   ////////// PROBLEM 4 //////////
   
@@ -66,7 +87,7 @@ function Employee(name, email, hireDate) {
     Write a prototype method for the Movie constructor function called changeRating.
     This method should take in a number as a parameter that will be a new rating.
     Find the average between the old rating and the new rating.
-    Change the rating property to become this new number and return the updated rating.
+    Change the rating property to become this new number and return the updated rating. ??????
   */
   
   function Movie(name, genre, rating) {
@@ -77,6 +98,14 @@ function Employee(name, email, hireDate) {
   
   // Code here
   
+Movie.prototype.changeRating = function(num){
+  this.rating = (num + this.rating)/2
+  return this.rating
+  }
+
+
+
+
   ////////// PROBLEM 5 //////////
   
   // Write a constructor function called User. This function should take in 4 parameters called name, age, email, and savedPosts in that order. Name and email will be strings, age will be a number and savedPosts will be an array of objects. These objects will each have 3 properties: id (a number), title (a string), and rating (a number between 1 and 5). These objects are the posts that the user will have saved to their account.
@@ -84,6 +113,17 @@ function Employee(name, email, hireDate) {
   // Once the User constructor function is created, write a prototype method for the User function. Name this method addSavedPost. It should take in three parameters: id (a number), title (a string) and rating (a number). Use these parameters to create a new object and add it to the savedPosts array. Make sure to name the properties the same as described previously (id, title, rating).
   
   // Code here
+
+  function User(name, age, email, savedPosts){
+    this.name = name;
+    this.age = age;
+    this.email = email;
+    this.savedPosts = savedPosts;
+  }
+
+  User.prototype.addSavedPost = function(id, title, rating){
+    this.savedPosts.push({id, title, rating})
+  }
   
   ////////// PROBLEM 6 //////////
   
@@ -92,10 +132,27 @@ function Employee(name, email, hireDate) {
   
   // Code here
   
+  User.prototype.removeSavedPost = function(id){
+    this.savedPosts.pop({id})
+  }
+  
   ////////// PROBLEM 7 //////////
   
   // You will continue to use the constructor function you created in problem 5.
   // Write a prototype method for the User constructor function named changePostRating that will take in two number parameters. The first will be an id (a number) and the second will be the new rating (a number). Use the id to find the matching object in the savedPosts array. Once you find the matching object, update it's rating score with the new rating parameter.
   
   // Code here
+
+  // User.prototype.changePostRating = function(id, newRating){
+  //   this.savedPosts.map((e) => e.id === id ? e.rating = newRating : null)
+  // }
+
+  User.prototype.changePostRating = function(id, newRating){
+    this.savedPosts.map((e) => {
+      e.id === id ? e.rating = newRating : null
+    return e
+  })
+  }
+
+
   
--- context.js
+++ context.js
@@ -19,6 +19,15 @@
 
 //Code Here
 
+let user = {
+  username: "bob",
+  email: "",
+  getUsername: function(){
+    return this.username
+  }
+}
+console.log(user.getUsername())
+
 ////////// PROBLEM 2 //////////
 
 /*
@@ -39,6 +48,8 @@ function Animal(name, species, food) {
 
 //Code Here
 
+let animal1 = new Animal("Baracuda", "Fish", "Flesh")
+
 ////////// PROBLEM 3 //////////
 
 /*
@@ -57,6 +68,8 @@ let who = {
 
 //Code Here
 
+const whoSaysHi = sayHi.bind(who)
+
 ////////// PROBLEM 4 //////////
 
 /*
@@ -68,7 +81,7 @@ function whatIsThis() {
 }
 
 // uncomment the line below and tell us what the context of "this" is for whatIsThis()
-//let context1 = ???
+let context1 = this;
 
 let product = {
   name: 'snake plant',
@@ -80,7 +93,8 @@ let product = {
 let func = whatIsThis.bind(product)
 
 // uncomment the line below and tell us what the context of "this" is when we invoke func
-//let context2 = ???
+
+let context2 = product
 
 let vacation = {
   location: 'Hawaii',
@@ -91,7 +105,7 @@ let vacation = {
 }
 
 // uncomment the line below and tell us what the context of "this" is when we invoke vacation.whatIsThis
-//let context3 = ???
+let context3 = vacation
 
 function Family(numParents, numKids, numPets) {
   this.numParents = numParents
@@ -105,5 +119,6 @@ function Family(numParents, numKids, numPets) {
 
 let family1 = new Family(2, 4, 1)
 
+console.log(family1.whatIsThis)
 // uncomment the line below and tell us what the context of "this" is for the instance of Family created above.
-// let context4 = ???
+let context4 = family1
